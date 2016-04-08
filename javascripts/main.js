106aec74244f56d632dac4010f384ce760103821
Qing Zhang <peachovo@gmail.com>
2016-04-08 22:25:45 +0800

    Mobile layout of DEMOS page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,7 @@
 window.onload = function () {
-    var viewportW = window.innerWidth || document.documentElement.clientWeight;
-    var skilltitles = document.querySelectorAll('.skill-title');
+    var viewportW = window.innerWidth || document.documentElement.clientWeight,
+        prelinks = document.querySelectorAll('.pre-links'),
+        skilltitles = document.querySelectorAll('.skill-title');
     if (viewportW <= 768) {
         skilltitles[2].textContent = 'JS';
         skilltitles[3].textContent = '工具';

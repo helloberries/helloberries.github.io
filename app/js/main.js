d69a11cadd374d76d2b3e448c81c3541942a09eb
helloberries <helloberriesqz@gmail.com>
2016-06-21 15:55:44 +0800

    complete PC version

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,28 @@
-window.onload = function () {
-	console.log('red');
-	console.log('hello');
-};
+/* Change Page */
+
+jQuery('.flexslider').flexslider({
+	animation: "fade",
+	start: function(slider) {
+		jQuery('.np-controls a.next').click(function(event){
+			event.preventDefault();
+			slider.flexAnimate(slider.getTarget("next"));
+		});
+		jQuery('.np-controls a.previous').click(function(event){
+			event.preventDefault();
+			slider.flexAnimate(slider.getTarget("previous"));
+		});
+	}
+});
+
+	$('.navigation li').click(function () {
+		var navid = '#'+$(this).attr('id'),
+				sectionid = navid.substr(0,navid.length-3);
+
+		$('.flexslider').css('height', $(sectionid).outerHeight());
+
+		$(navid).siblings().removeClass('flex-active');
+		$(sectionid).siblings().removeClass('flex-active-slide');
+
+		$(navid).addClass('flex-active');
+		$(sectionid).addClass('flex-active-slide');
+	});
